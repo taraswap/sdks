c404fd70be3ed5a2a8bab815383731e81dec24e6
Bogdan Popa <bogdan@kreatorhood.com>
2024-06-19 19:52:34 +0300

    fix(universal-router-sdk): add taraxa testnet mock router config

--- sdks/universal-router-sdk/src/utils/constants.ts
+++ sdks/universal-router-sdk/src/utils/constants.ts
@@ -114,6 +114,11 @@ const CHAIN_CONFIGS: { [key: number]: ChainConfig } = {
     weth: '0x5aea5775959fbc2557cc8789bc1bf90a239d9a91',
     creationBlock: 12640979,
   },
+  [842]: {
+    router: '0x28731BCC616B5f51dD52CF2e4dF0E78dD1136C06',
+    weth: '0x5aea5775959fbc2557cc8789bc1bf90a239d9a91',
+    creationBlock: 12640979,
+  }
 }
 
 export const UNIVERSAL_ROUTER_ADDRESS = (chainId: number): string => {
